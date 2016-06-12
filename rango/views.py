5843022b1ab0343df51cf37d5249952fb20fd491
Jon <Jon>
2016-06-12 20:31:11 +0800

    Initial commit

---
+++ rango/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rango.models import Category, Page
+
+# Create your views here.
+def index(request):
+    #con_text = {'boldmessage':'i am bold', 'another':'i am another'}
+    #return render(request, 'rango/index.html', con_text)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html')
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
