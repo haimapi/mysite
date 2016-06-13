d734feeb5040b3faf402609b6c5c34074c7305a2
haimapi <haimapi@163.com>
2016-06-13 21:22:43 +0800

    20160613

--- rango/forms.py
+++ rango/forms.py
@@ -12,11 +12,14 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name', )
 
+
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    # An inline class to provide additional information on the form
     class Meta:
+        # Provide an association between ModelForm and a model
         model = Page
         exclude = ('category', )
